e1783951abef43531c13616469a1a5718ef35424
Tijndagamer <tijndagamer25@gmail.com>
2017-02-07 21:27:57 +0100

    Accidentally pushed test file

--- src/argquotetest.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-arguments = 'hallo dit l\'ecole zijn "argumenten met quotes" erin echt kaulo kak"'
-print(arguments)
-arguments_split = arguments.split('"')
-print(arguments_split)
-arguments_list = []
-current_arg = ""
-print(arguments_list)
