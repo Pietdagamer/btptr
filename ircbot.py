d8cfd675318d9959935b08d0c893a3af233a4d3e
Tijndagamer <tijndagamer25@gmail.com>
2017-02-07 20:03:41 +0100

    Add parser methods

--- ircbot.py
+++ ircbot.py
@@ -23,14 +23,18 @@
 import socket
 import time
 import random
+import re
 
 class ircbot:
-    
     nickname = None
     channel = None
     owner = None
     irc_server_address = None
     irc_server_port = None
+
+    # compiled regexes for optimisation
+    command_regex = re.compile('PRIVMSG \#\S+ \:\!.*')
+    quote_regex = re.compile('"')
 
     def __init__(self, nickname, channel, owner, irc_server_address, irc_server_port):
         self.nickname = nickname
@@ -56,15 +60,41 @@ class ircbot:
         # Maybe use a regex for this?
         return msg.split("!")[0].split(':')[1]
 
+    def parse_arguments(self, data):
+        """Parse all the arguments of an user-issued command
+
+        Returns [command, [arguments]]
+        data -- received raw data
+        """
+
+        # Example data:
+        # :MrTijn!~MrTijn@unaffiliated/tijndagamer PRIVMSG #btjchmpy :!say this is a command
+
+        # Retrieve the command and its arguments from the data
+        command_and_args = command_regex.findall(data)[0].lstrip("PRIVMSG " + self.channel + ":!")
+        command = command_and_args.split(' ')[0]
+
+        # Second lstrip is to remove trailing whitespace
+        arguments = command_and_args.lstrip(command).lstrip()
+        if quote_regex.match(arguments) == None:
+            # No arguments with quotes given, so this is easy
+            arguments = arguments.split(' ')
+        else:
+            # todo
+        return [command, arguments]
+
     def parse_recv_data(self, data, sock):
         if data[0:4] == "PING":
             sock.send(data.replace("PING", "PONG"))
         if data[0] != ':':
             continue
         if data.split(' ')[1] == "PRIVMSG":
-            command = data.split(' ')[3]
-            if command.startswith(":!"):
-                # todo: implement other stuff here
+            msg = data.split(' ')[3]
+            if msg.startswith(":!"):
+                arguments = self.parse_arguments(data)
+
+                if command == ":!say":
+                    self.command_say(arguments, sock)
 
     # Methods for user-commands
 
