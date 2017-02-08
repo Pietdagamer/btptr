6106521bc656e92621ff0346471f0e385a17ca50
Tijndagamer <tijndagamer25@gmail.com>
2017-02-08 21:51:33 +0100

    Minor cleanup

--- src/main.py
+++ src/main.py
@@ -3,15 +3,11 @@
 
 import socket
 import time
-import random
 import ircbot
 
 DEBUG = True
 
-nicknumb = random.randint(0,999)
-
 bot_owner = "Meandonlymeandnooneelse"
-#nick = "btptr" + str(nicknumb)
 nick = "btjchmpy"
 
 if DEBUG:
@@ -23,7 +19,6 @@ server = "irc.freenode.net"
 port = 6667
 
 btptr = ircbot.IRCBot(nick, channel, bot_owner, server, port)
-
 sock = socket.socket()
 btptr.connect(sock)
 
